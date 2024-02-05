bba64b60ab9654b45330d9f48a586afa8652eb75
Crowdbotics <team@crowdbotics.com>
2024-02-05 23:42:40 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "onpremappazuremigra-46022",
+  url: "https://onpremappazuremigra-46022.botics.co",
+  api: "https://onpremappazuremigra-46022.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
